666dfc4ee0ad70f4444bcc707edf219bcd130b04
Its-Delimas <spencerdelimas@gmail.com>
2026-02-02 23:17:39 +0300

    Add alpha.html and alpha.js files with basic JavaScript functionality

---
+++ Javascript/basics/alpha.js
@@ -0,0 +1,8 @@
+function displayTime(){
+    document.getElementById('demo').innerHTML = Date();
+}
+    
+    //print screen
+document.getElementById("print").addEventListener('click', function (){
+    window.print();
+})
