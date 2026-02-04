b1890f6feaffc829eb15f0af22793735d9a640c3
Its-Delimas <spencerdelimas@gmail.com>
2026-02-05 01:26:39 +0300

    Add user registration form with validation and animation features

--- Javascript/basics/alpha.js
+++ Javascript/basics/alpha.js
@@ -1,8 +1,16 @@
-function displayTime(){
-    document.getElementById('demo').innerHTML = Date();
+function myMove (){
+    let id = null;
+    const elem = document.getElementById('animation')
+    let pos = 0;
+        clearInterval(id);
+        id = setInterval(frameElement, 5);
+            function frame(){
+                if (pos == 350){
+                    clearInterval(id);
+                } else {
+                    pos++;
+                    elem.style.top = pos + 'px';
+                    elem.style.left = pos + 'px';
+                }
+            }
 }
-    
-    //print screen
-document.getElementById("print").addEventListener('click', function (){
-    window.print();
-})
